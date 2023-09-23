b672f5594046d008a8b322eaf68deeb53eb0f3e4
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-09-23 16:26:59 +0100

    Add model and minimal EHR querying

--- query.py
+++ query.py
@@ -1,11 +1,14 @@
-from operator import itemgetter
-
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores import FAISS
+
+from model import DATABASE_URL, Patient
+
+from sqlmodel import create_engine, Session
+ENGINE = create_engine(DATABASE_URL)
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -37,8 +40,13 @@ class ChatSession:
 
 
 def fetch_and_parse_ehr(patient_id) -> list[str]:
-    return ["Jannic is in the hospital."]
+    with Session(ENGINE) as session:
+        patient = session.get(Patient, patient_id)  # Replace Patient with your actual Patient model
+        if patient:
+            return patient.ehr.split("---")
+        else:
+            raise ValueError(f"Patient with {patient_id} has no EHR")
 
 
-session = ChatSession(1234)
-print(session.query("Where is Jannic right now?"))
+session = ChatSession(1)
+print(session.query("How was the patient on day 5?"))
